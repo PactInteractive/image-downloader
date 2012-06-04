e1180f8cd4c6585987c0dcdd96945a6d6b6b52f3
Vladimir Sabev <vdsabev@gmail.com>
2012-06-05 00:29:29 +0300

    Numerous changes
    - Added image count next to the "All" header
    - Added JPEG format to regex
    - Updated description
    - Switched to tabs instead of spaces

--- popup.js
+++ popup.js
@@ -1,173 +1,176 @@
 window.onload = function() {
-  initializePopup();
-  initializeStyles();
+	initializePopup();
+	initializeStyles();
 };
 
 function initializePopup() {
-  document.getElementById('filter_textbox').onkeyup = filterImages;
-  document.getElementById('regex_checkbox').onchange = filterImages;
-  document.getElementById('toggle_all_checkbox').onchange = toggleAll;
-  document.getElementById('download_button').onclick = downloadCheckedImages;
-  
-  chrome.windows.getCurrent(function (currentWindow) {
-    chrome.tabs.query({ active: true, windowId: currentWindow.id }, function(activeTabs) {
-      chrome.tabs.executeScript(activeTabs[0].id, { file: 'send_images.js', allFrames: true });
-    });
-  });
+	document.getElementById('filter_textbox').onkeyup = filterImages;
+	document.getElementById('regex_checkbox').onchange = filterImages;
+	document.getElementById('toggle_all_checkbox').onchange = toggleAll;
+	document.getElementById('download_button').onclick = downloadCheckedImages;
+	
+	chrome.windows.getCurrent(function (currentWindow) {
+		chrome.tabs.query({ active: true, windowId: currentWindow.id }, function(activeTabs) {
+			chrome.tabs.executeScript(activeTabs[0].id, { file: 'send_images.js', allFrames: true });
+		});
+	});
 }
 
 function initializeStyles() {
-  jss('body', { width: (localStorage.body_width || localStorage.body_width_default) + 'px' });
-  jss('img', {
-      'min-width': (localStorage.image_min_width || localStorage.image_min_width_default) + 'px',
-      'max-width': (localStorage.image_max_width || localStorage.image_max_width_default) + 'px',
-      'border-width': (localStorage.image_border_width || localStorage.image_border_width_default) + 'px',
-      'border-style': localStorage.image_border_style || localStorage.image_border_style_default,
-      'border-color': localStorage.image_border_color || localStorage.image_border_color_default
-  });
-  jss('#filters_container', {
-    'border-bottom-width': (localStorage.image_border_width || localStorage.image_border_width_default) + 'px',
-    'border-bottom-style': localStorage.image_border_style || localStorage.image_border_style_default,
-    'border-bottom-color': localStorage.image_border_color || localStorage.image_border_color_default
-  });
+	jss('body', { width: (localStorage.body_width || localStorage.body_width_default) + 'px' });
+	jss('img', {
+			'min-width': (localStorage.image_min_width || localStorage.image_min_width_default) + 'px',
+			'max-width': (localStorage.image_max_width || localStorage.image_max_width_default) + 'px',
+			'border-width': (localStorage.image_border_width || localStorage.image_border_width_default) + 'px',
+			'border-style': localStorage.image_border_style || localStorage.image_border_style_default,
+			'border-color': localStorage.image_border_color || localStorage.image_border_color_default
+	});
+	jss('#filters_container', {
+		'border-bottom-width': (localStorage.image_border_width || localStorage.image_border_width_default) + 'px',
+		'border-bottom-style': localStorage.image_border_style || localStorage.image_border_style_default,
+		'border-bottom-color': localStorage.image_border_color || localStorage.image_border_color_default
+	});
 }
 
 var allImages = [];
 var visibleImages = [];
 
 function showImages() {
-  var images_table = document.getElementById('images_table');
-  while (images_table.children.length > 1) {
-    images_table.removeChild(images_table.children[images_table.children.length - 1])
-  }
-  
-  for (var i = 0; i < visibleImages.length; i++) {
-    var checkbox = document.createElement('input');
-    checkbox.checked = true;
-    checkbox.type = 'checkbox';
-    checkbox.id = 'checkbox' + i;
-    var col0 = document.createElement('td');
-    col0.appendChild(checkbox);
-    
-    var image = document.createElement('img');
-    image.src = visibleImages[i];
-    image.index = i;
-    image.onclick = function() {
-      var checkbox = document.getElementById('checkbox' + this.index);
-      checkbox.checked = !checkbox.checked;
-    };
-    
-    var col1 = document.createElement('td');
-    col1.appendChild(image);
-    
-    var anchor = document.createElement('a');
-    anchor.id = 'anchor' + i;
-    anchor.href = visibleImages[i];
-    anchor.download = '';
-    
-    var col2 = document.createElement('td');
-    col2.appendChild(anchor);
-    
-    var row = document.createElement('tr');
-    row.appendChild(col0);
-    row.appendChild(col1);
-    row.appendChild(col2);
-    
-    images_table.appendChild(row);
-  }
+	var images_table = document.getElementById('images_table');
+	while (images_table.children.length > 1) {
+		images_table.removeChild(images_table.children[images_table.children.length - 1])
+	}
+	
+	for (var i = 0; i < visibleImages.length; i++) {
+		var checkbox = document.createElement('input');
+		checkbox.checked = true;
+		checkbox.type = 'checkbox';
+		checkbox.id = 'checkbox' + i;
+		var col0 = document.createElement('td');
+		col0.appendChild(checkbox);
+		
+		var image = document.createElement('img');
+		image.src = visibleImages[i];
+		image.index = i;
+		image.onclick = function() {
+			var checkbox = document.getElementById('checkbox' + this.index);
+			checkbox.checked = !checkbox.checked;
+		};
+		
+		var col1 = document.createElement('td');
+		col1.appendChild(image);
+		
+		var anchor = document.createElement('a');
+		anchor.id = 'anchor' + i;
+		anchor.href = visibleImages[i];
+		anchor.download = '';
+		
+		var col2 = document.createElement('td');
+		col2.appendChild(anchor);
+		
+		var row = document.createElement('tr');
+		row.appendChild(col0);
+		row.appendChild(col1);
+		row.appendChild(col2);
+		
+		images_table.appendChild(row);
+	}
+	
+	var toggle_all_checkbox_label = document.getElementById('toggle_all_checkbox_label')
+	toggle_all_checkbox_label.innerHTML = 'All (' + visibleImages.length + ')';
 }
 
 //Toggle the checked state of all visible images
 function toggleAll() {
-  var checked = document.getElementById('toggle_all_checkbox').checked;
-  for (var i = 0; i < visibleImages.length; i++) {
-    document.getElementById('checkbox' + i).checked = checked;
-  }
+	var checked = document.getElementById('toggle_all_checkbox').checked;
+	for (var i = 0; i < visibleImages.length; i++) {
+		document.getElementById('checkbox' + i).checked = checked;
+	}
 }
 
 //Download all visible checked images
 function downloadCheckedImages() {
-  var anyFilesSaved = false;
-  for (var i = 0; i < visibleImages.length; i++) {
-    if (document.getElementById('checkbox' + i).checked) {
-      anyFilesSaved = true;
-      document.getElementById('anchor' + i).click();
-    }
-  }
-  
-  if (anyFilesSaved && (localStorage.show_download_notification || localStorage.show_download_notification_default) == 'true') {
-    showDownloadNotification();
-  }
+	var anyFilesSaved = false;
+	for (var i = 0; i < visibleImages.length; i++) {
+		if (document.getElementById('checkbox' + i).checked) {
+			anyFilesSaved = true;
+			document.getElementById('anchor' + i).click();
+		}
+	}
+	
+	if (anyFilesSaved && (localStorage.show_download_notification || localStorage.show_download_notification_default) == 'true') {
+		showDownloadNotification();
+	}
 }
 
 function showDownloadNotification() {
-  var filters_container = document.getElementById('filters_container');
-  var notification_container = document.getElementById('notification_container');
-  if (!notification_container) {
-    notification_container = document.createElement('div');
-    notification_container.id = 'notification_container';
-    filters_container.appendChild(notification_container);
-  }
-  
-  notification_container.innerHTML =
-    '<div class="notification">' + localStorage.download_notification + '</div>' +
-    '<input type="button" value="OK" id="okay_button" />' +
-    '<input type="checkbox" id="dont_show_again_checkbox" />' +
-    '<label for="dont_show_again_checkbox">Don\'t show this again</label>'
-    ;
-  
-  var okay_button = document.getElementById('okay_button');
-  okay_button.onclick = function() {
-    var dont_show_again_checkbox = document.getElementById('dont_show_again_checkbox');
-    localStorage.show_download_notification = !dont_show_again_checkbox.checked;
-    filters_container.removeChild(notification_container);
-  };
+	var filters_container = document.getElementById('filters_container');
+	var notification_container = document.getElementById('notification_container');
+	if (!notification_container) {
+		notification_container = document.createElement('div');
+		notification_container.id = 'notification_container';
+		filters_container.appendChild(notification_container);
+	}
+	
+	notification_container.innerHTML =
+		'<div class="notification">' + localStorage.download_notification + '</div>' +
+		'<input type="button" value="OK" id="okay_button" />' +
+		'<input type="checkbox" id="dont_show_again_checkbox" />' +
+		'<label for="dont_show_again_checkbox">Don\'t show this again</label>'
+		;
+	
+	var okay_button = document.getElementById('okay_button');
+	okay_button.onclick = function() {
+		var dont_show_again_checkbox = document.getElementById('dont_show_again_checkbox');
+		localStorage.show_download_notification = !dont_show_again_checkbox.checked;
+		filters_container.removeChild(notification_container);
+	};
 }
 
 //Re-filter allImages into visibleImages and reshow visibleImages
 function filterImages() {
-  var filterValue = document.getElementById('filter_textbox').value;
-  if (document.getElementById('regex_checkbox').checked) {
-    visibleImages = allImages.filter(function(image) {
-      return image.match(filterValue);
-    });
-  }
-  else {
-    var terms = filterValue.split(' ');
-    visibleImages = allImages.filter(function(image) {
-      for (var termI = 0; termI < terms.length; termI++) {
-        var term = terms[termI];
-        if (term.length != 0) {
-          var expected = (term[0] != '-');
-          if (!expected) {
-            term = term.substr(1);
-            if (term.length == 0) {
-              continue;
-            }
-          }
-          var found = (-1 !== image.indexOf(term));
-          if (found != expected) {
-            return false;
-          }
-        }
-      }
-      return true;
-    });
-  }
-  showImages();
+	var filterValue = document.getElementById('filter_textbox').value;
+	if (document.getElementById('regex_checkbox').checked) {
+		visibleImages = allImages.filter(function(image) {
+			return image.match(filterValue);
+		});
+	}
+	else {
+		var terms = filterValue.split(' ');
+		visibleImages = allImages.filter(function(image) {
+			for (var termI = 0; termI < terms.length; termI++) {
+				var term = terms[termI];
+				if (term.length != 0) {
+					var expected = (term[0] != '-');
+					if (!expected) {
+						term = term.substr(1);
+						if (term.length == 0) {
+							continue;
+						}
+					}
+					var found = (-1 !== image.indexOf(term));
+					if (found != expected) {
+						return false;
+					}
+				}
+			}
+			return true;
+		});
+	}
+	showImages();
 }
 
-//Add images to allImages and visibleImages, sort and show them.  send_images.js is
+//Add images to allImages and visibleImages, sort and show them.	send_images.js is
 //injected into all frames of the active tab, so this listener may be called multiple times
 chrome.extension.onRequest.addListener(function(images) {
-  for (var index in images) {
-    allImages.push(images[index]);
-  }
-  
-  if (localStorage.sort_images == 'true') {
-    allImages.sort();
-  }
-  
-  visibleImages = allImages;
-  showImages();
+	for (var index in images) {
+		allImages.push(images[index]);
+	}
+	
+	if (localStorage.sort_images == 'true') {
+		allImages.sort();
+	}
+	
+	visibleImages = allImages;
+	showImages();
 });
--- send_images.js
+++ send_images.js
@@ -1,37 +1,48 @@
 var images = [].slice.apply(document.getElementsByTagName('*'));
 images = images.map(function(element) {
-  if (element.tagName.toLowerCase() == 'img') {
-    var src = element.src;
-    var hashIndex = src.indexOf('#');
-    if (hashIndex >= 0) {
-      src = src.substr(0, hashIndex);
-    }
-    return src;
-  }
-  
-  if (element.style['background-image']) {
-    return element.style['background-image'].replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
-  }
-  
-  return '';
+	if (element.tagName.toLowerCase() == 'img') {
+		var src = element.src;
+		var hashIndex = src.indexOf('#');
+		if (hashIndex >= 0) {
+			src = src.substr(0, hashIndex);
+		}
+		return src;
+	}
+	
+	var IMAGE_REGEX = /(?:([^:\/?#]+):)?(?:\/\/([^\/?#]*))?([^?#]*\.(?:jpe?g|gif|png))(?:\?([^#]*))?(?:#(.*))?/;
+	if (element.tagName.toLowerCase() == 'a') {
+		var href = element.href;
+		if (IMAGE_REGEX.test(href)) {
+			return href;
+		}
+	}
+	
+	var backgroundImage = element.style['background-image'];
+	if (backgroundImage) {
+		var parsedURL = backgroundImage.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+		if (IMAGE_REGEX.test(parsedURL)) {
+			return parsedURL;
+		}
+	}
+	
+	return '';
 });
-
 
 images = removeDuplicateOrEmpty(images);
 chrome.extension.sendRequest(images);
 
 function removeDuplicateOrEmpty(a) {
-  var index,
-      result = [],
-      hash = {};
-  
-  for (index = 0; index < a.length; index++) {
-    hash[a[index]] = 0;
-  }
-  for (key in hash) {
-    if (key != '') {
-      result.push(key);
-    }
-  }
-  return result;
+	var index,
+			result = [],
+			hash = {};
+	
+	for (index = 0; index < a.length; index++) {
+		hash[a[index]] = 0;
+	}
+	for (key in hash) {
+		if (key != '') {
+			result.push(key);
+		}
+	}
+	return result;
 }
