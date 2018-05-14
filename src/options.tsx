f674306ab0de409a84705f2c9d76ff59bf983f61
Vladimir Sabev <vdsabev@gmail.com>
2018-05-14 18:36:27 +0300

    Implemented notifications

--- src/options.tsx
+++ src/options.tsx
@@ -9,17 +9,22 @@ import { Actions } from './options/Actions';
 import { Filters, FiltersOptions } from './options/Filters';
 import { General, GeneralOptions } from './options/General';
 import { Images, ImagesOptions } from './options/Images';
-import { Notifications } from './options/Notifications';
+import { Notification, Notifications } from './options/Notifications';
 import { SetOption } from './options/SetOption';
 import { css } from './style';
 
-class State implements GeneralOptions, FiltersOptions, ImagesOptions {
+interface State {
+  notifications: Notification[];
+  options: Options;
+}
+
+class Options implements GeneralOptions, FiltersOptions, ImagesOptions {
   static save(values: Record<string, number | boolean>, saveValue: (key: string, value: string) => any): void {
     Object.keys(values).forEach((key) => saveValue(key, values[key].toString()));
   }
 
-  static load(savedValues: Record<string, string>, defaultValues: any): State {
-    return Object.keys(defaultValues).reduce<State>(
+  static load(savedValues: Record<string, string>, defaultValues: any): Options {
+    return Object.keys(defaultValues).reduce<Options>(
       (result, key) => {
         const defaultValue = defaultValues[key];
         const savedValue = savedValues[key];
@@ -37,7 +42,7 @@ class State implements GeneralOptions, FiltersOptions, ImagesOptions {
     );
   }
 
-  // Filters
+  // Values
   folder_name = '';
   new_file_name = '';
   filter_url = '';
@@ -76,39 +81,12 @@ class State implements GeneralOptions, FiltersOptions, ImagesOptions {
 }
 
 class App extends Component<{}, State> {
-  private readonly defaultState = new State();
-  readonly state = State.load(localStorage, this.defaultState);
+  private readonly notificationDuration = 1000; // TODO: Make 10000
+  private readonly defaultOptions = new Options();
 
-  private saveOptions = () => {
-    State.save(this.state as any, localStorage.setItem.bind(localStorage));
-    this.addNotification('Options saved.', 'success');
-  };
-
-  private resetOptions = () => {
-    this.setState(this.defaultState);
-    this.addNotification(
-      'All options have been reset to their default values. You can now save the changes you made or discard them by closing this page.',
-      'warning'
-    );
-  };
-
-  private clearData = () => {
-    const result = window.confirm(
-      'Are you sure you want to clear all data for this extension? This includes filters, options and the name of the default folder where files are saved.'
-    );
-    if (result) {
-      localStorage.clear();
-      this.setState(this.defaultState);
-      this.addNotification('All date cleared.', 'success');
-    }
-  };
-
-  private setOption: SetOption = (key, value) => {
-    this.setState({ [key]: value } as any);
-  };
-
-  private addNotification = (message: string, type: 'success' | 'warning' | 'error') => {
-    // TODO: Implement
+  readonly state = {
+    notifications: [],
+    options: Options.load(localStorage, this.defaultOptions),
   };
 
   render(props: {}, state: State) {
@@ -117,16 +95,58 @@ class App extends Component<{}, State> {
         <h2>Image Downloader</h2>
 
         <About />
-        <General options={state} setOption={this.setOption} />
-        <Filters options={state} setOption={this.setOption} />
-        <Images options={state} setOption={this.setOption} />
+        <General options={state.options} setOption={this.setOption} />
+        <Filters options={state.options} setOption={this.setOption} />
+        <Images options={state.options} setOption={this.setOption} />
 
         <Actions save={this.saveOptions} reset={this.resetOptions} clear={this.clearData} />
 
-        <Notifications />
+        <Notifications notifications={state.notifications} />
       </div>
     );
   }
+
+  private saveOptions = () => {
+    Options.save(this.state.options as any, localStorage.setItem.bind(localStorage));
+    this.addNotification({ type: 'success', message: 'Options saved.' });
+  };
+
+  private resetOptions = () => {
+    this.setState({ options: this.defaultOptions });
+    this.addNotification({
+      type: 'warning',
+      message:
+        'All options have been reset to their default values. You can now save the changes you made or discard them by closing this page.',
+    });
+  };
+
+  private clearData = () => {
+    const result = window.confirm(
+      'Are you sure you want to clear all data for this extension? This includes filters, options and the name of the default folder where files are saved.'
+    );
+    if (result) {
+      localStorage.clear();
+      this.setState({ options: this.defaultOptions });
+      this.addNotification({ type: 'success', message: 'All data cleared.' });
+    }
+  };
+
+  private setOption: SetOption = (key, value) => {
+    this.setState((state: State) => ({ ...state, options: { ...state.options, [key]: value } }));
+  };
+
+  private addNotification = (notificationData: Pick<Notification, 'type' | 'message'>) => {
+    const notification: Notification = { ...notificationData, timestamp: Date.now() };
+
+    this.setState((state: State) => ({ ...state, notifications: [...state.notifications, notification] }));
+
+    setTimeout(() => {
+      this.setState((state: State) => ({
+        ...state,
+        notifications: state.notifications.filter((n) => n !== notification),
+      }));
+    }, this.notificationDuration);
+  };
 }
 
 render(<App />, document.body);
--- src/options/Notifications.tsx
+++ src/options/Notifications.tsx
@@ -1,5 +1,30 @@
-import { h } from '../dom';
+import { Component, h } from '../dom';
 
-export const Notifications = (props: {}) => (
-  <div id="notifications"></div>
+export interface Notification {
+  type: 'success' | 'warning' | 'error';
+  message: string;
+  timestamp: number;
+}
+
+export const Notifications = (props: { notifications: Notification[] }) => (
+  <div>
+    {props.notifications.map((notification) => <NotificationView notification={notification} />)}
+  </div>
 );
+
+// TODO: Implement `fade-leave` animation
+class NotificationView extends Component<{ notification: Notification }, {}> {
+  componentDidMount() {
+    if (!this.base) return;
+
+    const el = this.base;
+    el.classList.add('fade-enter');
+    setTimeout(() => {
+      el.classList.add('fade-enter-active');
+    }, 0);
+  }
+
+  render(props: { notification: Notification }, state: {}) {
+    return <div key={props.notification.timestamp} >{props.notification.message}</div>;
+  }
+}
