18bd9f3bf097969d0645a475337b2336224aecd8
Vlad Sabev <vdsabev@gmail.com>
2021-02-02 01:26:16 +0200

    Fix script scoping bug

--- src/send_images.js
+++ src/send_images.js
@@ -1,113 +1,115 @@
-const imageDownloader = {
-  // Source: https://support.google.com/webmasters/answer/2598805?hl=en
-  imageRegex: /(?:([^:\/?#]+):)?(?:\/\/([^\/?#]*))?([^?#]*\.(?:bmp|gif|jpe?g|png|svg|webp))(?:\?([^#]*))?(?:#(.*))?/i,
+(() => {
+  const imageDownloader = {
+    // Source: https://support.google.com/webmasters/answer/2598805?hl=en
+    imageRegex: /(?:([^:\/?#]+):)?(?:\/\/([^\/?#]*))?([^?#]*\.(?:bmp|gif|jpe?g|png|svg|webp))(?:\?([^#]*))?(?:#(.*))?/i,
 
-  extractImagesFromTags() {
-    return [].slice
-      .apply(document.querySelectorAll('img, a, [style]'))
-      .map(imageDownloader.extractImageFromElement);
-  },
+    extractImagesFromTags() {
+      return [].slice
+        .apply(document.querySelectorAll('img, a, [style]'))
+        .map(imageDownloader.extractImageFromElement);
+    },
 
-  extractImagesFromStyles() {
-    const imagesFromStyles = [];
-    for (let i = 0; i < document.styleSheets.length; i++) {
-      const styleSheet = document.styleSheets[i];
-      // Prevents `Failed to read the 'cssRules' property from 'CSSStyleSheet': Cannot access rules` error. Also see:
-      // https://github.com/vdsabev/image-downloader/issues/37
-      // https://github.com/odoo/odoo/issues/22517
-      if (styleSheet.hasOwnProperty('cssRules')) {
-        const { cssRules } = styleSheet;
-        for (let j = 0; j < cssRules.length; j++) {
-          const style = cssRules[j].style;
-          if (style && style.backgroundImage) {
-            const url = imageDownloader.extractURLFromStyle(
-              style.backgroundImage
-            );
-            if (imageDownloader.isImageURL(url)) {
-              imagesFromStyles.push(url);
+    extractImagesFromStyles() {
+      const imagesFromStyles = [];
+      for (let i = 0; i < document.styleSheets.length; i++) {
+        const styleSheet = document.styleSheets[i];
+        // Prevents `Failed to read the 'cssRules' property from 'CSSStyleSheet': Cannot access rules` error. Also see:
+        // https://github.com/vdsabev/image-downloader/issues/37
+        // https://github.com/odoo/odoo/issues/22517
+        if (styleSheet.hasOwnProperty('cssRules')) {
+          const { cssRules } = styleSheet;
+          for (let j = 0; j < cssRules.length; j++) {
+            const style = cssRules[j].style;
+            if (style && style.backgroundImage) {
+              const url = imageDownloader.extractURLFromStyle(
+                style.backgroundImage
+              );
+              if (imageDownloader.isImageURL(url)) {
+                imagesFromStyles.push(url);
+              }
             }
           }
         }
       }
-    }
 
-    return imagesFromStyles;
-  },
+      return imagesFromStyles;
+    },
 
-  extractImageFromElement(element) {
-    if (element.tagName.toLowerCase() === 'img') {
-      let src = element.src;
-      const hashIndex = src.indexOf('#');
-      if (hashIndex >= 0) {
-        src = src.substr(0, hashIndex);
+    extractImageFromElement(element) {
+      if (element.tagName.toLowerCase() === 'img') {
+        let src = element.src;
+        const hashIndex = src.indexOf('#');
+        if (hashIndex >= 0) {
+          src = src.substr(0, hashIndex);
+        }
+        return src;
       }
-      return src;
-    }
 
-    if (element.tagName.toLowerCase() === 'a') {
-      const href = element.href;
-      if (imageDownloader.isImageURL(href)) {
-        imageDownloader.linkedImages[href] = '0';
-        return href;
+      if (element.tagName.toLowerCase() === 'a') {
+        const href = element.href;
+        if (imageDownloader.isImageURL(href)) {
+          imageDownloader.linkedImages[href] = '0';
+          return href;
+        }
       }
-    }
 
-    const backgroundImage = window.getComputedStyle(element).backgroundImage;
-    if (backgroundImage) {
-      const parsedURL = imageDownloader.extractURLFromStyle(backgroundImage);
-      if (imageDownloader.isImageURL(parsedURL)) {
-        return parsedURL;
+      const backgroundImage = window.getComputedStyle(element).backgroundImage;
+      if (backgroundImage) {
+        const parsedURL = imageDownloader.extractURLFromStyle(backgroundImage);
+        if (imageDownloader.isImageURL(parsedURL)) {
+          return parsedURL;
+        }
       }
-    }
 
-    return '';
-  },
+      return '';
+    },
 
-  extractURLFromStyle(url) {
-    return url.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
-  },
+    extractURLFromStyle(url) {
+      return url.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+    },
 
-  isImageURL(url) {
-    return (
-      url.indexOf('data:image') === 0 || imageDownloader.imageRegex.test(url)
-    );
-  },
+    isImageURL(url) {
+      return (
+        url.indexOf('data:image') === 0 || imageDownloader.imageRegex.test(url)
+      );
+    },
 
-  relativeUrlToAbsolute(url) {
-    return url.indexOf('/') === 0 ? `${window.location.origin}${url}` : url;
-  },
+    relativeUrlToAbsolute(url) {
+      return url.indexOf('/') === 0 ? `${window.location.origin}${url}` : url;
+    },
 
-  removeDuplicateOrEmpty(images) {
-    const hash = {};
-    for (let i = 0; i < images.length; i++) {
-      hash[images[i]] = 0;
-    }
-
-    const result = [];
-    for (let key in hash) {
-      if (key !== '') {
-        result.push(key);
+    removeDuplicateOrEmpty(images) {
+      const hash = {};
+      for (let i = 0; i < images.length; i++) {
+        hash[images[i]] = 0;
       }
-    }
 
-    return result;
-  },
-};
+      const result = [];
+      for (let key in hash) {
+        if (key !== '') {
+          result.push(key);
+        }
+      }
 
-imageDownloader.linkedImages = {}; // TODO: Avoid mutating this object in `extractImageFromElement`
-imageDownloader.images = imageDownloader.removeDuplicateOrEmpty(
-  []
-    .concat(
-      imageDownloader.extractImagesFromTags(),
-      imageDownloader.extractImagesFromStyles()
-    )
-    .map(imageDownloader.relativeUrlToAbsolute)
-);
+      return result;
+    },
+  };
 
-chrome.runtime.sendMessage({
-  linkedImages: imageDownloader.linkedImages,
-  images: imageDownloader.images,
-});
+  imageDownloader.linkedImages = {}; // TODO: Avoid mutating this object in `extractImageFromElement`
+  imageDownloader.images = imageDownloader.removeDuplicateOrEmpty(
+    []
+      .concat(
+        imageDownloader.extractImagesFromTags(),
+        imageDownloader.extractImagesFromStyles()
+      )
+      .map(imageDownloader.relativeUrlToAbsolute)
+  );
 
-imageDownloader.linkedImages = null;
-imageDownloader.images = null;
+  chrome.runtime.sendMessage({
+    linkedImages: imageDownloader.linkedImages,
+    images: imageDownloader.images,
+  });
+
+  imageDownloader.linkedImages = null;
+  imageDownloader.images = null;
+})();
