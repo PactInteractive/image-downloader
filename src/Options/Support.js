4d412657f12fd7042144bc6e68790255968e83aa
Vlad Sabev <vdsabev@gmail.com>
2022-10-14 11:24:41 +0300

    Make BTC the only option to support the project

--- src/Options/Support.js
+++ src/Options/Support.js
@@ -1,172 +1,67 @@
 import html from '../html.js';
 import { ExternalLink } from '../components/ExternalLink.js';
 
-const numberOfActiveUsers = '1,200,000+';
+const numberOfActiveUsers = '1,000,000+';
 const years = new Date().getFullYear() - 2012;
+const addresses = {
+  btc: '19UsTZPYtNp1h2y4QT2a7gbNYNZrEdjXvE',
+};
 
 export const SupportList = () => html`
-  <p>
-    If you're one of the ${numberOfActiveUsers} people using this extension,
-    over the past${' '} ${years} years marketing companies have approached me
-    with offers to pay in exchange for <b>your private data</b> like:
-  </p>
+  <div style=${{ display: 'flex', gap: '24px' }}>
+    <div style=${{ flex: 2 }}>
+      <p>
+        If you're one of the ${numberOfActiveUsers} people using this extension,
+        over the past ${years} years marketing companies have approached me with
+        offers to pay in exchange for <b>your private data</b> like:
+      </p>
 
-  <ul>
-    <li>what websites you visit</li>
-    <li>when you visit them</li>
-    <li>where you visit them from</li>
-  </ul>
+      <ul>
+        <li>what websites you visit</li>
+        <li>when you visit them</li>
+        <li>where you visit them from</li>
+      </ul>
 
-  <p>
-    And what do these companies think the value of your data is?
-    <b> 0.15¢ to 0.45¢ a month.</b>
-    <br />
-    Less than a penny! Do you think that's worth compromising your privacy and
-    trust for?
-  </p>
-
-  <p>My answer has always been a resounding <b>NO!</b></p>
-
-  <p>
-    If you agree, please consider supporting this project so I can continue
-    working on it while keeping the extension <b>free</b>, <b>open-source</b>,
-    and <b>without targeted ads or tracking algorithms</b> - as it always has
-    been.
-  </p>
-
-  <div class="tab-list">
-    <div class="tab-item">
-      <${SupportRadio} id="support_patreon" value="patreon" />
-
-      <label class="tab-header" for="support_patreon">
-        <img src="/images/patreon.png" />
-        Patreon
-      </label>
-
-      <div class="tab-content centered">
-        <${ExternalLink} href="https://www.patreon.com/vdsabev">
-          <br /><br />
-          <img src="/images/patreon-wordmark.png" />
-          <br /><br />
-          patreon.com/vdsabev
-        <//>
-
-        <p>
-          Give recurring monthly donations and participate in our Discord
-          community!
-        </p>
-      </div>
-    </div>
-
-    <div class="tab-item">
-      <${SupportRadio} id="support_paypal" value="paypal" />
-
-      <label class="tab-header" for="support_paypal">
-        <img src="/images/paypal.png" />
-        PayPal
-      </label>
-
-      <div class="tab-content centered">
-        <${ExternalLink} href="https://www.paypal.me/vdsabev">
-          <img src="/images/paypal-wordmark.jpg" />
-          paypal.me/vdsabev
-        <//>
-
-        <p>Give a one-time donation to show your support for the project.</p>
-      </div>
-    </div>
-
-    <div class="tab-item">
-      <${SupportRadio} id="support_btc" value="btc" />
-
-      <label class="tab-header" for="support_btc">
-        <img src="/images/btc.png" />
-        BTC
-      </label>
-
-      <div class="tab-content centered">
+      <p>
+        And what do they think your privacy and trust are worth?
         <br />
-        <${ExternalLink} href="bitcoin:3LGkKmET7sGzsJriW16mtM8Kmo2XN7258C">
-          <img src="/images/btc-qr.png" />
-        <//>
-
-        <pre>3LGkKmET7sGzsJriW16mtM8Kmo2XN7258C</pre>
-
-        <p style=${{ maxWidth: '380px' }}>
-          Any bitcoin you send will be retained as bitcoin and hodled 💥👀
-        </p>
-      </div>
-    </div>
-
-    <div class="tab-item">
-      <${SupportRadio} id="support_eth" value="eth" />
-
-      <label class="tab-header" for="support_eth">
-        <img src="/images/eth.png" />
-        ETH
-      </label>
-
-      <div class="tab-content centered">
+        <b>0.15 to 0.45 cents</b>
         <br />
+        Less than a penny!
+      </p>
+
+      <p>
+        My response to such offers?
+        <br />
+        Mark as spam and delete!
+      </p>
+
+      <p>
+        If you want to, you may support this project by donating Bitcoin.
+        <br />
+        In any case, the extension will remain <b>free</b>, <b>open-source</b>,
+        and <b> without targeted ads or tracking algorithms</b>.
+      </p>
+
+      <p>
+        The source code can be found on GitHub:${' '}
         <${ExternalLink}
-          href="ethereum:0x49707Cb358e8B2F795C8FceF4D2DcfD2BADF7679"
-        >
-          <img src="/images/eth-qr.png" />
-        <//>
-
-        <pre>0x49707Cb358e8B2F795C8FceF4D2DcfD2BADF7679</pre>
-
-        <p>
-          Any Ethereum you send will be retained as Ethereum and hodled
-          <br />
-          or used as gas for running decentralized applications 💥👀
-        </p>
-      </div>
+          href="https://github.com/PactInteractive/image-downloader"
+        />
+      </p>
     </div>
 
-    <div class="tab-item">
-      <${SupportRadio} id="support_bat" value="bat" />
+    <div style=${{ flex: 1, textAlign: 'center' }}>
+      <h2 style=${{ display: 'inline-flex', gap: '0.5ch' }}>
+        <img src="/images/btc.png" style=${{ height: '1em' }} />
+        Bitcoin address
+      </h2>
 
-      <label class="tab-header" for="support_bat">
-        <img src="/images/bat.png" />
-        BAT
-      </label>
+      <${ExternalLink} href="bitcoin:${addresses.btc}">
+        <img src="/images/btc-qr.png" style=${{ width: '100%' }} />
+      <//>
 
-      <div class="tab-content centered">
-        <br />
-        <${ExternalLink}
-          href="ethereum:0x0d8775f648430679a709e98d2b0cb6250d2887ef/transfer?address=0xdb54EBD0eF147599050B3629d65a73d65ef344D2"
-        >
-          <img src="/images/bat-qr.png" />
-        <//>
-
-        <pre>0xdb54EBD0eF147599050B3629d65a73d65ef344D2</pre>
-
-        <div>
-          <p>
-            Alternatively, you can send a tip on my GitHub profile page:
-            <br />
-            <${ExternalLink} href="https://github.com/vdsabev" />
-          </p>
-
-          <p>
-            Any BAT you send will be retained as BAT and hodled
-            <br />
-            or used to tip other creators 💥👀
-          </p>
-        </div>
-      </div>
+      <pre>${addresses.btc}</pre>
     </div>
   </div>
-
-  <p>
-    The source code can be found on GitHub:${' '}
-    <${ExternalLink}
-      href="https://github.com/PactInteractive/image-downloader"
-    />
-  </p>
-`;
-
-const SupportRadio = (props) => html`
-  <input type="radio" name="support" ...${props} />
 `;
